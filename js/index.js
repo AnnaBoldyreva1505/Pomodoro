0c84be2d26b6454b55c787c71455d75b3d2843ff
Anna Boldyreva <104367856+AnnaBoldyreva1505@users.noreply.github.com>
2023-02-02 23:11:37 +0600

    todo

--- js/index.js
+++ js/index.js
@@ -1,14 +1,16 @@
-import { initControl } from "./control.js"
-import { state } from "./state.js"
+import { initControl } from "./control.js";
+import { state } from "./state.js";
+import { initToDo } from "./todo.js";
 
 const initPomodoro = () => {
-    initControl()
+  initControl();
+  initToDo();
 
-    state.activeTodo = {
-        id: 'default',
-        pomodoro: 0,
-        title: "Помодоро",
-    }
-}
+   state.activeTodo = {
+      id: 'default',
+     pomodoro: 0,
+      title: "Помодоро",
+   }
+};
 
-initPomodoro()
+initPomodoro();
